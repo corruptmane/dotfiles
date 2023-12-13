16ef5822329067b34c6fea52216e1b7ee082c585
corruptmane <corruptmane@gmail.com>
2023-12-13 16:30:46 +0200

    `nvim` added

---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,14 @@
+require('onedark').setup {
+	style = 'cool',
+	transparent = true,
+
+}
+require('onedark').load()
+require('lualine').setup {
+    options = {
+        icons_enabled = true,
+        theme = 'onedark',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+    }
+}
---
+++ .config/nvim/lua/corrupt/init.lua
@@ -0,0 +1,3 @@
+require("corrupt.plug")
+require("corrupt.set")
+require("corrupt.remap")
