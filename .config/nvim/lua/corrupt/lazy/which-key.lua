aad7cdcd6d4ea5d87aecd6ac69c36a6eb3df013e
corruptmane <corruptmane@gmail.com>
2024-04-13 21:36:20 +0300

    screenshot can now be taken from visual mode using `<leader>cs`

--- .config/nvim/lua/corrupt/lazy/which-key.lua
+++ .config/nvim/lua/corrupt/lazy/which-key.lua
@@ -4,18 +4,28 @@ return {
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function()
-      require('which-key').setup()
+      local wk = require('which-key')
+      wk.setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>p'] = { name = '[P]roject', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      }
+      wk.register(
+        {
+          ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        },
+        { mode = 'v' }
+      )
+      wk.register(
+        {
+          ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+          ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+          ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+          ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+          ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+          ['<leader>p'] = { name = '[P]roject', _ = 'which_key_ignore' },
+          ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        },
+        { mode = 'n' }
+      )
     end,
   },
 
