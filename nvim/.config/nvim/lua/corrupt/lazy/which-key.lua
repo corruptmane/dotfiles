7c1bdea9f370982b3ae1d6c24bca69788912ff4f
corruptmane <corruptmane@gmail.com>
2024-09-11 05:51:37 +0300

    update: updating according to new `which-key` release

--- nvim/.config/nvim/lua/corrupt/lazy/which-key.lua
+++ nvim/.config/nvim/lua/corrupt/lazy/which-key.lua
@@ -3,30 +3,23 @@ return {
   { -- Useful plugin to show pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function()
-      local wk = require('which-key')
-      wk.setup()
-
-      -- Document existing key chains
-      wk.register(
-        {
-          ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        },
-        { mode = 'v' }
-      )
-      wk.register(
-        {
-          ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-          ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-          ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-          ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-          ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-          ['<leader>p'] = { name = '[P]roject', _ = 'which_key_ignore' },
-          ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        },
-        { mode = 'n' }
-      )
-    end,
+    -- config = function()
+    --   local wk = require('which-key')
+    --   wk.setup()
+    --
+    --   -- Document existing key chains
+    -- end,
+    opts = {
+      spec = {
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>p', group = '[P]roject' },
+        { '<leader>g', group = '[G]it', mode = { 'n', 'v' } },
+      }
+    }
   },
 
 }
