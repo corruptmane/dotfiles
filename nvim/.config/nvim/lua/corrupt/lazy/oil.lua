2a2584e557f13abb6c6c9fb4d3b71c9358c88f61
corruptmane <corruptmane@gmail.com>
2024-10-20 18:21:56 +0300

    update: manually map `oil`

--- nvim/.config/nvim/lua/corrupt/lazy/oil.lua
+++ nvim/.config/nvim/lua/corrupt/lazy/oil.lua
@@ -6,5 +6,17 @@ return {
       opts = {}
     }
   },
-  opts = {},
+  opts = {
+    use_default_keymaps = false,
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["<C-p>"] = "actions.preview",
+      ["<C-c>"] = "actions.close",
+      ["<C-r>"] = "actions.refresh",
+      ["-"] = "actions.parent",
+      ["gs"] = "actions.change_sort",
+      ["g."] = "actions.toggle_hidden",
+    },
+  },
 }
