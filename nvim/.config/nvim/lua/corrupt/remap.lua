d0e194567d259cccf8948df40bf0a8391794c94d
corruptmane <corruptmane@gmail.com>
2024-10-20 18:20:32 +0300

    fix+feat: fix auto indent mapping | add tab management mappings

--- nvim/.config/nvim/lua/corrupt/remap.lua
+++ nvim/.config/nvim/lua/corrupt/remap.lua
@@ -11,10 +11,8 @@ map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic messa
 map('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-map('n', '<leader>pv', function () vim.cmd('Ex') end, { desc = 'Open [P]roject [V]iew' })
-
-map('v', 'J', "<cmd>m '>+1<CR>gv=gv", { desc = 'Move line down, automatically indent' })
-map('v', 'K', "<cmd>m '<-2<CR>gv=gv", { desc = 'Move line up, automatically indent' })
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move line down, automatically indent' })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move line up, automatically indent' })
 
 map('n', '<C-d>', '<C-d>zz', { desc = 'Navigate down and center screen automatically' })
 map('n', '<C-u>', '<C-u>zz', { desc = 'Navigate up and center screen automatically' })
@@ -31,3 +29,8 @@ map('n', '<leader>|', '<cmd>vsp<CR>', { desc = 'Open vertical split' })
 map('n', '<leader>-', '<cmd>sp<CR>', { desc = 'Open horizontal split' })
 
 map('n', '-', ':Oil<CR>')
+
+map('n', '<leader>tc', vim.cmd.tabnew, { desc = 'Create new tab' })
+map('n', '<leader>tq', vim.cmd.tabc, { desc = 'Quit current tab' })
+map('n', '<leader>tn', vim.cmd.tabn, { desc = 'Next tab' })
+map('n', '<leader>tN', vim.cmd.tabN, { desc = 'Previous tab' })
